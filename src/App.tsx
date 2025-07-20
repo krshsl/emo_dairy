9ac4ed656d65e7ceb438c317d4ae93a10890ac7e
Krishna Sathyamurthy <64264567+krshsl@users.noreply.github.com>
2025-07-20 11:48:42 -0400

    init react & tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white p-10 text-center text-4xl font-bold">
+      <h1>TAILWIND TEST</h1>
+      <p>If this text is white on a red background, Tailwind is working!</p>
+    </div>
+  );
+}
+
+export default App;
